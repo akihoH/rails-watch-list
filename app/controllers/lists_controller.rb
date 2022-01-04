68b8cdccc64b0fb55806f53dd808e63b59fa7bc1
Akiho Hamamoto <ahamamoto1125@gmail.com>
2022-01-04 13:48:59 +0900

    added bookmarks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,7 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @movies = Bookmark.where(list_id = )
+    @movies = Bookmark.all
   end
 
   def new
